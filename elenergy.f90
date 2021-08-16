2cf5bdc5bd4b5e60909c45e2433e412c519b983e
Jacek Jakowski <jjakowski@gmail.com>
2021-08-16 18:21:43 -0400

    Transferring  files from  dftb-src5 (local)  to src

--- elenergy.f90
+++
@@ -1,9 +0,0 @@
-
-subroutine   ElEnergy(nbasis,nat,energy,Pn,Sn,Hn,Gn,gammat) 
-       integer       ::  nbasis,nat
-       real (kind=8) ::   Pn(nbasis,nbasis), Sn(nbasis,nbasis), Hn(nbasis,nbasis), Gn(nbasis,nbasis)
-
-
-
-
-end subroutinr elEnergy
